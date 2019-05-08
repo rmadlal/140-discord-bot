d32969310253b9225663510d029dfb9f3fa2a9ea
Ron <ronmadlal@gmail.com>
2019-05-08 16:59:35 +0300

    Add reaction to existing 140 reaction, fetch emoji on connection, handle errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import sys
 
 CONFIG = 'config.json'
 ONEFORTY = '140'
@@ -14,21 +15,39 @@ class Bot(object):
 
         @self._client.event
         async def on_ready():
-            print(f'Connected to {self._client.guilds}')
+            try:
+                guild = await self._client.fetch_guild(184830159805743104)
+                self._140_emoji = next((e for e in await guild.fetch_emojis() if e.name == ONEFORTY), None)
+            except discord.DiscordException as e:
+                print(f'Error: {str(e)}', file=sys.stderr)
+                return
 
         @self._client.event
         async def on_message(message):
             if message.author == self._client.user:
                 return
+            if self._140_emoji is None:
+                # Why am I here? I have no purpose in this server.
+                return
             if message.content.find(ONEFORTY) == -1:
                 return
-            if self._140_emoji is None:
-                self._140_emoji = next((e for e in await message.guild.fetch_emojis() if e.name == ONEFORTY), None)
-                if self._140_emoji is None:
-                    # Why am I here? I have no purpose in this server.
-                    return
             print(f'Sending reaction on message: {message.content}')
-            await message.add_reaction(self._140_emoji)
+            try:
+                await message.add_reaction(self._140_emoji)
+            except discord.DiscordException as e:
+                print(f'Reaction failed: {str(e)}', file=sys.stderr)
+
+        @self._client.event
+        async def on_reaction_add(reaction, user):
+            if user == self._client.user:
+                return
+            if self._140_emoji is None or reaction.emoji != self._140_emoji:
+                return
+            print(f'Adding reaction')
+            try:
+                await reaction.message.add_reaction(self._140_emoji)
+            except discord.DiscordException as e:
+                print(f'Reaction failed: {str(e)}', file=sys.stderr)
 
     def run(self):
         self._client.run(self._config['token'])
