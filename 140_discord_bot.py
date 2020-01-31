b8fff4cb36b8b4ca122c4b6e36139bd03d1db9dc
Ron <ronmadlal@gmail.com>
2020-01-31 19:08:36 +0200

    Use on_raw_reaction_add

--- 140_discord_bot.py
+++ 140_discord_bot.py
@@ -3,11 +3,13 @@ import logging
 import os
 import sys
 import time
+from collections import namedtuple
+from operator import attrgetter
 from threading import Thread
 from typing import Callable, Final, Optional, Union
 
 import schedule
-from discord import Client, DiscordException, Emoji, Game, Message, Reaction, User, TextChannel
+from discord import Client, DiscordException, Emoji, Game, Message, RawReactionActionEvent, TextChannel, User
 
 TOKEN: Final = os.getenv('BOT_TOKEN')
 _140_EMOJI_ID: Final = 447884638049009686
@@ -15,6 +17,8 @@ _140_IRL_CHANNEL_ID: Final = 329682110019534849
 RACES_CHANNEL_ID: Final = 187112305505468417
 ZET_ID: Final = 123160659130056708
 GAMEGUY_ID: Final = 93504619614834688
+
+Reaction = namedtuple('ReactionFromRaw', ['emoji', 'message', 'user'])
 
 
 class Bot:
@@ -33,14 +37,15 @@ class Bot:
 
         @self._client.event
         async def on_ready():
-            Thread(target=self.ping_racers_every_day).start()
+            Thread(target=self._ping_racers_every_day).start()
 
         @self._client.event
         async def on_message(message: Message):
             await self._140_reaction(message, *self._on_message_conditions)
 
         @self._client.event
-        async def on_reaction_add(reaction: Reaction, _):
+        async def on_raw_reaction_add(payload: RawReactionActionEvent):
+            reaction = await self._init_reaction_from_raw(payload)
             await self._140_reaction(reaction, *self._on_reaction_add_conditions)
 
     @property
@@ -60,13 +65,6 @@ class Bot:
         return self._client.get_user(GAMEGUY_ID)
 
     @staticmethod
-    def _get_message(model: Union[Message, Reaction]) -> Message:
-        if isinstance(model, Message):
-            return model
-        if isinstance(model, Reaction):
-            return model.message
-
-    @staticmethod
     def _has_140_in_order(reaction: Reaction) -> bool:
         one, four, zero = '1️⃣', '4️⃣', '0️⃣'
         reaction_emojis = [reaction.emoji for reaction in reaction.message.reactions]
@@ -75,9 +73,21 @@ class Bot:
         except ValueError:
             return False
 
+    @staticmethod
+    def _get_message(model: Union[Message, Reaction]) -> Message:
+        if isinstance(model, Message):
+            return model
+        if isinstance(model, Reaction):
+            return model.message
+
+    async def _init_reaction_from_raw(self, payload: RawReactionActionEvent) -> Reaction:
+        message = await self._client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        user = self._client.get_user(payload.user_id)
+        return Reaction(payload.emoji, message, user)
+
     def _should_respond(self, model: Union[Message, Reaction]) -> bool:
-        return (isinstance(model, Message) and model.author != self._client.user) \
-               or (isinstance(model, Reaction) and not model.me)
+        get_user = {Message: attrgetter('author'), Reaction: attrgetter('user')}[type(model)]
+        return get_user(model) != self._client.user
 
     async def _140_reaction(self, model: Union[Message, Reaction] = None, *conditions: Callable):
         if not (self._should_respond(model) and any(cond(model) for cond in conditions)):
@@ -88,7 +98,7 @@ class Bot:
         except DiscordException as e:
             print(f'Reaction failed: {e}', file=sys.stderr)
 
-    def ping_racers_every_day(self):
+    def _ping_racers_every_day(self):
         # wrap Channel.send() in a non-async function because schedule doesn't work with async
         def send(message):
             asyncio.run_coroutine_threadsafe(self._races_channel.send(message), self._client.loop)
